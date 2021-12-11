a6fbd7462f5ba277fe58c7f1f4ec51fb90c3c6d9
Victor Jurado <victor.nemejur@gmail.com>
2021-12-11 19:21:33 -0300

    Commit inicial

---
+++ Empresa.UI.Windows/ClienteIncluirForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Empresa.UI.Windows
+{
+    partial class ClienteIncluirForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.telefoneTextBox = new System.Windows.Forms.TextBox();
+            this.incluirButton = new System.Windows.Forms.Button();
+            this.voltarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefone:";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(119, 23);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 26);
+            this.idTextBox.TabIndex = 5;
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.Location = new System.Drawing.Point(119, 71);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(248, 26);
+            this.nomeTextBox.TabIndex = 6;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(119, 122);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(248, 26);
+            this.emailTextBox.TabIndex = 7;
+            // 
+            // telefoneTextBox
+            // 
+            this.telefoneTextBox.Location = new System.Drawing.Point(119, 174);
+            this.telefoneTextBox.Name = "telefoneTextBox";
+            this.telefoneTextBox.Size = new System.Drawing.Size(248, 26);
+            this.telefoneTextBox.TabIndex = 8;
+            // 
+            // incluirButton
+            // 
+            this.incluirButton.Location = new System.Drawing.Point(181, 230);
+            this.incluirButton.Name = "incluirButton";
+            this.incluirButton.Size = new System.Drawing.Size(90, 36);
+            this.incluirButton.TabIndex = 10;
+            this.incluirButton.Text = "Incluir";
+            this.incluirButton.UseVisualStyleBackColor = true;
+            // 
+            // voltarButton
+            // 
+            this.voltarButton.Location = new System.Drawing.Point(277, 230);
+            this.voltarButton.Name = "voltarButton";
+            this.voltarButton.Size = new System.Drawing.Size(90, 36);
+            this.voltarButton.TabIndex = 11;
+            this.voltarButton.Text = "Voltar";
+            this.voltarButton.UseVisualStyleBackColor = true;
+            // 
+            // ClienteIncluirForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 286);
+            this.Controls.Add(this.voltarButton);
+            this.Controls.Add(this.incluirButton);
+            this.Controls.Add(this.telefoneTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.nomeTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ClienteIncluirForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Incluir Cliente";
+            this.Load += new System.EventHandler(this.ClienteIncluirForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox telefoneTextBox;
+        private System.Windows.Forms.Button incluirButton;
+        private System.Windows.Forms.Button voltarButton;
+    }
+}
---
+++ Empresa.UI.Windows/ClientesForm.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Empresa.UI.Windows
+{
+    partial class ClientesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.novoButton = new System.Windows.Forms.Button();
+            this.alterarButton = new System.Windows.Forms.Button();
+            this.excluirButton = new System.Windows.Forms.Button();
+            this.sairButton = new System.Windows.Forms.Button();
+            this.confirmarIncluirButton = new System.Windows.Forms.Button();
+            this.confirmarAlterarbutton = new System.Windows.Forms.Button();
+            this.confirmarExcluirButton = new System.Windows.Forms.Button();
+            this.conteudoPanel = new System.Windows.Forms.Panel();
+            this.listaDataGridView = new System.Windows.Forms.DataGridView();
+            this.fichaPanel = new System.Windows.Forms.Panel();
+            this.telefoneTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.voltarButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.conteudoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listaDataGridView)).BeginInit();
+            this.fichaPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 356);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(812, 62);
+            this.panel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.novoButton);
+            this.flowLayoutPanel1.Controls.Add(this.alterarButton);
+            this.flowLayoutPanel1.Controls.Add(this.excluirButton);
+            this.flowLayoutPanel1.Controls.Add(this.sairButton);
+            this.flowLayoutPanel1.Controls.Add(this.confirmarIncluirButton);
+            this.flowLayoutPanel1.Controls.Add(this.confirmarAlterarbutton);
+            this.flowLayoutPanel1.Controls.Add(this.confirmarExcluirButton);
+            this.flowLayoutPanel1.Controls.Add(this.voltarButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Padding = new System.Windows.Forms.Padding(10, 7, 0, 0);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(812, 62);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // novoButton
+            // 
+            this.novoButton.Location = new System.Drawing.Point(13, 10);
+            this.novoButton.Name = "novoButton";
+            this.novoButton.Size = new System.Drawing.Size(91, 34);
+            this.novoButton.TabIndex = 0;
+            this.novoButton.Text = "Novo";
+            this.novoButton.UseVisualStyleBackColor = true;
+            this.novoButton.Click += new System.EventHandler(this.novoButton_Click);
+            // 
+            // alterarButton
+            // 
+            this.alterarButton.Location = new System.Drawing.Point(110, 10);
+            this.alterarButton.Name = "alterarButton";
+            this.alterarButton.Size = new System.Drawing.Size(93, 34);
+            this.alterarButton.TabIndex = 1;
+            this.alterarButton.Text = "Alterar";
+            this.alterarButton.UseVisualStyleBackColor = true;
+            this.alterarButton.Click += new System.EventHandler(this.alterarButton_Click);
+            // 
+            // excluirButton
+            // 
+            this.excluirButton.Location = new System.Drawing.Point(209, 10);
+            this.excluirButton.Name = "excluirButton";
+            this.excluirButton.Size = new System.Drawing.Size(88, 34);
+            this.excluirButton.TabIndex = 2;
+            this.excluirButton.Text = "Excluir";
+            this.excluirButton.UseVisualStyleBackColor = true;
+            this.excluirButton.Click += new System.EventHandler(this.excluirButton_Click);
+            // 
+            // sairButton
+            // 
+            this.sairButton.Location = new System.Drawing.Point(303, 10);
+            this.sairButton.Name = "sairButton";
+            this.sairButton.Size = new System.Drawing.Size(93, 34);
+            this.sairButton.TabIndex = 3;
+            this.sairButton.Text = "Sair";
+            this.sairButton.UseVisualStyleBackColor = true;
+            this.sairButton.Click += new System.EventHandler(this.sairButton_Click);
+            // 
+            // confirmarIncluirButton
+            // 
+            this.confirmarIncluirButton.Location = new System.Drawing.Point(402, 10);
+            this.confirmarIncluirButton.Name = "confirmarIncluirButton";
+            this.confirmarIncluirButton.Size = new System.Drawing.Size(92, 34);
+            this.confirmarIncluirButton.TabIndex = 4;
+            this.confirmarIncluirButton.Text = "Gravar";
+            this.confirmarIncluirButton.UseVisualStyleBackColor = true;
+            this.confirmarIncluirButton.Click += new System.EventHandler(this.confirmarIncluirButton_Click);
+            // 
+            // confirmarAlterarbutton
+            // 
+            this.confirmarAlterarbutton.Location = new System.Drawing.Point(500, 10);
+            this.confirmarAlterarbutton.Name = "confirmarAlterarbutton";
+            this.confirmarAlterarbutton.Size = new System.Drawing.Size(92, 34);
+            this.confirmarAlterarbutton.TabIndex = 5;
+            this.confirmarAlterarbutton.Text = "Alterar";
+            this.confirmarAlterarbutton.UseVisualStyleBackColor = true;
+            this.confirmarAlterarbutton.Click += new System.EventHandler(this.confirmarAlterarbutton_Click);
+            // 
+            // confirmarExcluirButton
+            // 
+            this.confirmarExcluirButton.Location = new System.Drawing.Point(598, 10);
+            this.confirmarExcluirButton.Name = "confirmarExcluirButton";
+            this.confirmarExcluirButton.Size = new System.Drawing.Size(200, 34);
+            this.confirmarExcluirButton.TabIndex = 6;
+            this.confirmarExcluirButton.Text = "Confirmar Exclusão";
+            this.confirmarExcluirButton.UseVisualStyleBackColor = true;
+            this.confirmarExcluirButton.Click += new System.EventHandler(this.confirmarExcluirButton_Click);
+            // 
+            // conteudoPanel
+            // 
+            this.conteudoPanel.Controls.Add(this.fichaPanel);
+            this.conteudoPanel.Controls.Add(this.listaDataGridView);
+            this.conteudoPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.conteudoPanel.Location = new System.Drawing.Point(0, 0);
+            this.conteudoPanel.Name = "conteudoPanel";
+            this.conteudoPanel.Padding = new System.Windows.Forms.Padding(10);
+            this.conteudoPanel.Size = new System.Drawing.Size(812, 356);
+            this.conteudoPanel.TabIndex = 1;
+            // 
+            // listaDataGridView
+            // 
+            this.listaDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listaDataGridView.Location = new System.Drawing.Point(452, 35);
+            this.listaDataGridView.Name = "listaDataGridView";
+            this.listaDataGridView.RowHeadersWidth = 62;
+            this.listaDataGridView.RowTemplate.Height = 28;
+            this.listaDataGridView.Size = new System.Drawing.Size(342, 289);
+            this.listaDataGridView.TabIndex = 0;
+            // 
+            // fichaPanel
+            // 
+            this.fichaPanel.Controls.Add(this.telefoneTextBox);
+            this.fichaPanel.Controls.Add(this.emailTextBox);
+            this.fichaPanel.Controls.Add(this.nomeTextBox);
+            this.fichaPanel.Controls.Add(this.idTextBox);
+            this.fichaPanel.Controls.Add(this.label4);
+            this.fichaPanel.Controls.Add(this.label3);
+            this.fichaPanel.Controls.Add(this.label2);
+            this.fichaPanel.Controls.Add(this.label1);
+            this.fichaPanel.Location = new System.Drawing.Point(14, 35);
+            this.fichaPanel.Name = "fichaPanel";
+            this.fichaPanel.Size = new System.Drawing.Size(421, 289);
+            this.fichaPanel.TabIndex = 1;
+            // 
+            // telefoneTextBox
+            // 
+            this.telefoneTextBox.Location = new System.Drawing.Point(134, 198);
+            this.telefoneTextBox.Name = "telefoneTextBox";
+            this.telefoneTextBox.Size = new System.Drawing.Size(248, 26);
+            this.telefoneTextBox.TabIndex = 16;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(134, 146);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(248, 26);
+            this.emailTextBox.TabIndex = 15;
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.Location = new System.Drawing.Point(134, 95);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(248, 26);
+            this.nomeTextBox.TabIndex = 14;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(134, 47);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 26);
+            this.idTextBox.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Telefone:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nome:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Id:";
+            // 
+            // voltarButton
+            // 
+            this.voltarButton.Location = new System.Drawing.Point(13, 50);
+            this.voltarButton.Name = "voltarButton";
+            this.voltarButton.Size = new System.Drawing.Size(89, 34);
+            this.voltarButton.TabIndex = 7;
+            this.voltarButton.Text = "Voltar";
+            this.voltarButton.UseVisualStyleBackColor = true;
+            this.voltarButton.Click += new System.EventHandler(this.voltarButton_Click);
+            // 
+            // ClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 418);
+            this.Controls.Add(this.conteudoPanel);
+            this.Controls.Add(this.panel1);
+            this.Name = "ClientesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clientes";
+            this.Load += new System.EventHandler(this.ClientesForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.conteudoPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listaDataGridView)).EndInit();
+            this.fichaPanel.ResumeLayout(false);
+            this.fichaPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button novoButton;
+        private System.Windows.Forms.Button alterarButton;
+        private System.Windows.Forms.Button excluirButton;
+        private System.Windows.Forms.Button sairButton;
+        private System.Windows.Forms.Button confirmarIncluirButton;
+        private System.Windows.Forms.Button confirmarAlterarbutton;
+        private System.Windows.Forms.Button confirmarExcluirButton;
+        private System.Windows.Forms.Panel conteudoPanel;
+        private System.Windows.Forms.Panel fichaPanel;
+        private System.Windows.Forms.DataGridView listaDataGridView;
+        private System.Windows.Forms.TextBox telefoneTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button voltarButton;
+    }
+}
